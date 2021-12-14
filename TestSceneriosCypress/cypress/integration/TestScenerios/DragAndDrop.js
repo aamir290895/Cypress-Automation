3603f5f63834df3af794256da7ccfa040b2fa677
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-12-15 00:04:23 +0530

    14/12/21

--- TestSceneriosCypress/cypress/integration/TestScenerios/DragAndDrop.js
+++ TestSceneriosCypress/cypress/integration/TestScenerios/DragAndDrop.js
@@ -3,26 +3,31 @@ import '@4tw/cypress-drag-drop'
 describe('DradAndDrop' , ()=>{
 
 
+  beforeEach(() => {
 
 
-  beforeEach('BEFORE METHOD' , ()=>{
+    cy.visit('http://demo.guru99.com/test/drag_drop.html');
+     })
 
-
-     cy.visit('https://jqueryui.com/droppable/');
-     cy.get('.demo-frame').click;
-
-
-  }) 
-
-  it('Drag & Drop' , ()=>{
-
-    cy.wait(1000);
-    cy.get('#draggable').drag('#droppable');
+    it('Drag & Drop', () => {
+      const dataTransfer = new DataTransfer();
+      cy.wait(1000);
+      cy.get('#fourth').trigger('drag', { dataTransfer });
+      cy.get('#amt7').trigger('drop', { dataTransfer });
 
    
 
+    })
+
+    it('another method for drag & drop', () => {
+    
+      cy.get('#fourth').drag('#amt7');
+      cy.wait(2000);
+
+
+
+    })
+
+
+
   })
-
-
-
-})
